83c3fbea3e074136d880615af6c193a6f1e16cf9
johann-vu <68650868+johann-vu@users.noreply.github.com>
2026-09-25 20:06:00 +0200

    wip

--- vue-project/src/main.ts
+++ vue-project/src/main.ts
@@ -14,6 +14,9 @@ import '@awesome.me/webawesome/dist/components/input/input.js'
 import '@awesome.me/webawesome/dist/components/option/option.js'
 import '@awesome.me/webawesome/dist/components/page/page.js'
 import '@awesome.me/webawesome/dist/components/tag/tag.js'
+import '@awesome.me/webawesome/dist/components/spinner/spinner.js'
+import '@awesome.me/webawesome/dist/components/select/select.js'
+import '@awesome.me/webawesome/dist/components/tag/tag.js'
 import '@awesome.me/webawesome/dist/components/switch/switch.js'
 
 // Registriert Diagrammtypen und das Projekt-Theme für ECharts.
